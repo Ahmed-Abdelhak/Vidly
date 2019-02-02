5eb95f2d16e6de984cd75179bf292a40d01050da
Ahmed <engineer.ahmedhussein@gmail.com>
2019-02-02 15:37:04 +0200

    a small hybred list of customers and movies in tables

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        List<Customer> _customers = new List<Customer>
+        {
+            new Customer()
+            {
+                Name = "Ahmed",
+                  Id = 1
+            },
+            new Customer()
+            {
+                Name = "Mohamed"  ,
+                Id = 2
+            }
+
+        };
+        public ActionResult Index()
+        {
+           
+            return View(_customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var emp = _customers.Find(e=>e.Id == id);
+            return View(emp) ;
+        }
+
+       
+    }
+}
---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult index()
+        {
+            var movies = new List<Movie>
+            {
+              new Movie()
+              {
+                  Name = "shrek",
+              },
+                new Movie()
+                {
+                    Name = "Friends",
+                }
+            };
+        
+            return View(movies);
+        }
+    }
+}
